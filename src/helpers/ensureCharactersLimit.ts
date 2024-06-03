b978efafdef2340b479a298fdee1b5a548ed094c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-06-03 09:24:41 -0300

    fix: make ensureCharactersLimit always return string

--- src/helpers/ensureCharactersLimit.ts
+++ src/helpers/ensureCharactersLimit.ts
@@ -2,32 +2,39 @@ export const ensureCharactersLimit = (text: string, limit: number) => {
   if (text.length <= limit)
     return text
 
-  let charactersCounter = 0
   const words = text.split(' ')
-  for (let i = 0; i < words.length; i++) {
+  const specialEndCharacters = [
+    '.',
+    ',',
+    '!',
+    '?',
+    ';',
+    ':'
+  ]
+
+  let finalText = ''
+  let charactersCounter = 0
+  let i = 0
+
+  for (i = 0; i < words.length; i++) {
     charactersCounter += words[i]!.length
 
-    if (charactersCounter > limit){
+    if (charactersCounter > limit) {
       if (i < 3)
-        return text.substring(0, limit - 20) + '...'
+        finalText = text.substring(0, limit - 20)
 
-      let finalText = words.filter((_, index) => index < i).join(' ')
-      const lastCharacter = finalText[finalText.length - 1]!
-      const specialEndCharacters = [
-        '.',
-        ',',
-        '!',
-        '?',
-        ';',
-        ':'
-      ]
-
-      if (specialEndCharacters.includes(lastCharacter))
-        finalText += ' '
-
-      finalText += '...'
-
-      return finalText
+      break
     }
   }
+
+  if (!finalText)
+    finalText = words.filter((_, index) => index < i).join(' ')
+
+  const lastCharacter = finalText[finalText.length - 1]!
+  if (specialEndCharacters.includes(lastCharacter))
+    finalText += ' '
+
+  finalText += '...'
+
+  return finalText
 }
