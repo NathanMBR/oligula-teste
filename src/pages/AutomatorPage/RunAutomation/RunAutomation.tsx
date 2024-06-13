0d0a72ffd5f66512b8cee1d1597393281c69335d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-06-12 23:32:05 -0300

    feat: hide fab if has expanded step at RunAutomation.tsx

--- src/pages/AutomatorPage/RunAutomation/RunAutomation.tsx
+++ src/pages/AutomatorPage/RunAutomation/RunAutomation.tsx
@@ -1,20 +1,30 @@
 import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 import { IconSettingsAutomation } from '@tabler/icons-react'
 
 import { AutomationContext } from '../../../providers'
 import { FAB } from '../../../components'
 
 import { runAutomationScript } from './runAutomationScript'
+import type { AutomatorPageParams } from '../AutomatorPage'
 
 export const RunAutomation = () => {
   const automationPayload = useContext(AutomationContext)
+
+  const { expandedStepId: rawExpandedStepId } = useParams<AutomatorPageParams>()
+  const expandedStepId = Number(rawExpandedStepId)
+
+  const isHidden =
+    automationPayload.steps.length <= 0 ||
+    Number.isNaN(expandedStepId) ||
+    expandedStepId !== -1
 
   return (
     <FAB
       ActionIconProps={{ variant: 'filled' }}
       onClick={() => runAutomationScript(automationPayload)}
       icon={<IconSettingsAutomation />}
-      hidden={automationPayload.steps.length <= 0}
+      hidden={isHidden}
     />
   )
 }
