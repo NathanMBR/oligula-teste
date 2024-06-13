f0f51fcf0b24385a39c06abc82399733506d4921
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-06-12 23:21:59 -0300

    refactor: use derived state at Automation.tsx

--- src/pages/AutomatorPage/Automation/Automation.tsx
+++ src/pages/AutomatorPage/Automation/Automation.tsx
@@ -1,8 +1,8 @@
 import {
   useContext,
-  useEffect,
-  useState
+  useEffect
 } from 'react'
+import { useParams } from 'react-router-dom'
 import { Button } from '@mantine/core'
 import { IconPlus } from '@tabler/icons-react'
 
@@ -14,6 +14,7 @@ import type { StepData } from '../../../types'
 
 import { AutomationSteps } from './AutomationSteps'
 import { StepTypes } from '../StepTypes'
+import type { AutomatorPageParams } from '../AutomatorPage'
 
 export type AutomationProps = {
   setIsNewStepOpen: (value: boolean) => void
@@ -27,33 +28,61 @@ export const Automation = (props: AutomationProps) => {
     getStep,
     getStepPositionString
   } = useContext(AutomationContext)
+  const { setPageSubtitle } = useContext(HeaderContext)
 
-  const { setPageSubtitle: setCurrentSubtitle } = useContext(HeaderContext)
+  const { expandedStepId: rawExpandedStepId } = useParams<AutomatorPageParams>()
+  const expandedStepId = Number(rawExpandedStepId)
 
-  const [expandedStepId, setExpandedStepId] = useState<StepData['id']>(-1)
-  const [steps, setSteps] = useState(contextSteps)
+  const loadExpandedStep = (id: StepData['id']) => {
+    /* eslint-disable no-console */
+    if (Number.isNaN(id)) {
+      console.error(`Unexpected Error: Expected valid step id (got ${rawExpandedStepId})`)
+      return null
+    }
+
+    if (id < 0)
+      return null
+
+    const step = getStep(id)
+    if (!step) {
+      console.error(`Unexpected Error: Step with id ${id} not found`)
+      return null
+    }
+    /* eslint-enable no-console */
+
+    return step
+  }
+
+  const getChildrenSteps = (step: StepData | null) => {
+    if (!step)
+      return null
+
+    if (!('steps' in step.data)) {
+      // eslint-disable-next-line no-console
+      console.error(`Unexpected Error: Step with id ${step.id} has no children steps`)
+      return null
+    }
+
+    return step.data.steps
+  }
+
+  const steps = getChildrenSteps(loadExpandedStep(expandedStepId)) || contextSteps
 
   useEffect(() => {
-    if (expandedStepId >= 0) {
-      const step = getStep(expandedStepId)
-      if (!step) {
-        // eslint-disable-next-line no-console
-        console.error(`Unexpected Error: Step with id ${expandedStepId} not found`)
-        return
-      }
+    if (Number.isNaN(expandedStepId))
+      return
 
-      if (!('steps' in step.data)) {
-        // eslint-disable-next-line no-console
-        console.error(`Unexpected Error: Step with id ${expandedStepId} has no steps`)
-        return
-      }
+    if (expandedStepId < 0)
+      return
 
-      const stepPosition = getStepPositionString(expandedStepId)
+    const step = getStep(expandedStepId)
+    if (!step)
+      return
 
-      setCurrentSubtitle(`Passo ${stepPosition}: ${StepTypes[step.type].title}`)
-      setSteps(step.data.steps)
-    }
-  }, [expandedStepId])
+    const stepPosition = getStepPositionString(step.id)
+
+    setPageSubtitle(`Passo ${stepPosition}: ${StepTypes[step.type].title}`)
+  }, [rawExpandedStepId])
 
   return (
     <>
@@ -67,10 +96,7 @@ export const Automation = (props: AutomationProps) => {
         Adicionar passo
       </Button>
 
-      <AutomationSteps
-        steps={steps}
-        setExpandedStepId={setExpandedStepId}
-      />
+      <AutomationSteps steps={steps} />
     </>
   )
 }
