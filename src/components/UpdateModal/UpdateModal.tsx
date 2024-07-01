5a13d4a01dae90c98937686068ae52f2aef25516
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-07-01 09:40:20 -0300

    feat: handle multiple update statis in UpdateModal.tsx

--- src/components/UpdateModal/UpdateModal.tsx
+++ src/components/UpdateModal/UpdateModal.tsx
@@ -1,5 +1,7 @@
 import {
+  Alert,
   Button,
+  Code,
   Group,
   Loader,
   Modal,
@@ -9,39 +11,161 @@ import {
 } from '@mantine/core'
 import {
   useContext,
-  useEffect,
-  useState
+  useState,
+  type ReactElement
 } from 'react'
+import { IconInfoCircle } from '@tabler/icons-react'
 
 import { PreloadContext } from '../../providers'
-import { sleep } from '../../helpers'
+
+type UpdateModalBodyProps = {
+  handleClose: () => void
+  handleUpdate: () => void
+}
+
+const UpdateModalAvailableBody = (props: UpdateModalBodyProps) => {
+  const {
+    handleClose,
+    handleUpdate
+  } = props
+
+  return (
+    <>
+      <Text>Uma nova versão está disponível.</Text>
+      <Text fw={500}>Gostaria de baixá-la?</Text>
+
+      <Group mt='md' justify='flex-end'>
+        <Button
+          variant='default'
+          onClick={handleClose}
+        >
+          Não
+        </Button>
+
+        <Button onClick={handleUpdate}>Sim</Button>
+      </Group>
+    </>
+  )
+}
+
+const UpdateModalUpdatingBody = (props: UpdateModalBodyProps) => {
+  const {
+    handleClose,
+    handleUpdate
+  } = props
+
+  return (
+    <>
+      <Text>Baixando atualização...</Text>
+
+      <Group mt='md' justify='flex-end'>
+        <Button
+          variant='default'
+          onClick={handleClose}
+          disabled
+        >
+          Não
+        </Button>
+
+        <Button
+          onClick={handleUpdate}
+          disabled
+        >
+          <Loader size={20} />
+        </Button>
+      </Group>
+    </>
+  )
+}
+
+const UpdateModalSuccessBody = (props: Pick<UpdateModalBodyProps, 'handleClose'>) => {
+  const { handleClose } = props
+
+  return (
+    <>
+      <Text>Atualização concluída com sucesso.</Text>
+      <Text>Por favor, <span style={{ fontWeight: 'bold' }}>reinicie o programa</span> para aplicar as atualizações.</Text>
+
+      <Group mt='md' justify='flex-end'>
+        <Button onClick={handleClose}>Fechar</Button>
+      </Group>
+    </>
+  )
+}
+
+const UpdateModalFailureBody = (props: Pick<UpdateModalBodyProps, 'handleClose'>) => {
+  const { handleClose } = props
+
+  const { os } = useContext(PreloadContext)
+  const isLinux = os.type === 'Linux'
+
+  return (
+    <>
+      <Text mb='xl'>Um erro inesperado ocorreu durante a atualização.</Text>
+
+      {
+        isLinux
+          ? <Alert
+            title='Nota sobre o Linux'
+            color='yellow'
+            icon={<IconInfoCircle />}
+          >
+            <Text mb='xs'>A atualização automática não está disponível caso tenha instalado este aplicativo pelo pacote <Code>.deb</Code>.</Text>
+            <Text>Caso este seja o seu caso, baixe a atualização manualmente do repositório do GitHub, ou considere mudar para a versão <Code>AppImage</Code>.</Text>
+          </Alert>
+          : null
+      }
+
+      <Group mt='md' justify='flex-end'>
+        <Button onClick={handleClose}>Fechar</Button>
+      </Group>
+    </>
+  )
+}
 
 export const UpdateModal = () => {
+  type UpdateStates = 'AVAILABLE' | 'UPDATING' | 'SUCCESS' | 'FAILURE'
+
   const { update } = useContext(PreloadContext)
 
-  const [open, isOpen] = useState(update.available)
-  const [isUpdating, setIsUpdating] = useState(false)
+  const [open, isOpen] = useState(update.available || true as boolean)
+  const [updateState, setUpdateState] = useState<UpdateStates>('AVAILABLE')
 
   const handleClose = () => isOpen(false)
 
   const handleUpdate = () => {
     const executeUpdate = async () => {
-      await sleep(1000)
-      await update.execute()
+      const success = await update.execute()
+      return success
     }
 
-    setIsUpdating(true)
+    setUpdateState('UPDATING')
 
     executeUpdate()
-      .finally(handleClose)
+      .then(success => setUpdateState(success ? 'SUCCESS' : 'FAILURE'))
   }
 
-  useEffect(() => {
-    return () => {
-      if (update.unlisten)
-        update.unlisten()
-    }
-  }, [])
+  const isUpdating = updateState === 'UPDATING'
+
+  const modalBodies: Record<UpdateStates, ReactElement> = {
+    AVAILABLE: <UpdateModalAvailableBody
+      handleClose={handleClose}
+      handleUpdate={handleUpdate}
+    />,
+
+    UPDATING: <UpdateModalUpdatingBody
+      handleClose={handleClose}
+      handleUpdate={handleUpdate}
+    />,
+
+    SUCCESS: <UpdateModalSuccessBody
+      handleClose={handleClose}
+    />,
+
+    FAILURE: <UpdateModalFailureBody
+      handleClose={handleClose}
+    />
+  }
 
   return (
     <Modal.Root
@@ -56,7 +180,13 @@ export const UpdateModal = () => {
       <Modal.Content>
         <Modal.Header>
           <Modal.Title>
-            <Title order={3}>Atualização disponível</Title>
+            <Title order={4}>
+              {
+                updateState === 'FAILURE'
+                  ? 'Erro durante a atualização'
+                  : 'Atualização disponível'
+              }
+            </Title>
           </Modal.Title>
 
           <Transition mounted={!isUpdating}>
@@ -67,34 +197,7 @@ export const UpdateModal = () => {
         </Modal.Header>
 
         <Modal.Body>
-          {
-            isUpdating
-              ? <Text>Baixando atualização...</Text>
-              : <>
-                <Text>Uma nova versão está disponível.</Text>
-                <Text fw={500}>Gostaria de baixá-la?</Text>
-              </>
-          }
-
-          <Group mt='md' justify='flex-end'>
-            <Button
-              variant='default'
-              onClick={handleClose}
-              disabled={isUpdating}>
-              Não
-            </Button>
-
-            <Button
-              onClick={handleUpdate}
-              disabled={isUpdating}
-            >
-              {
-                isUpdating
-                  ? <Loader size={20} />
-                  : 'Sim'
-              }
-            </Button>
-          </Group>
+          {modalBodies[updateState]}
         </Modal.Body>
       </Modal.Content>
     </Modal.Root>
